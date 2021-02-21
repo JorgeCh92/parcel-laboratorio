2079f1d415d637a3e0eac8c02b853acb0cde1030
jorge ch <jorgecalle.92@gmail.com>
2021-02-21 14:09:35 +0100

    Typescript y React

---
+++ src/holaMundoReact.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const currentDate: string = new Date().toDateString();  
+const apiBase: string= `${process.env.API_BASE}`;
+ReactDOM.render(
+    <h1>Hola Mundo con React</h1>,
+    document.getElementById("holaMundoReact")
+);
+
+ReactDOM.render(
+    <span>
+        
+    </span>,
+    document.getElementById("currentDate")
+);
+
+ReactDOM.render(
+    <span>
+        {currentDate}
+    </span>,
+    document.getElementById("currentDate")
+);
+
+ReactDOM.render(
+    <span>
+        {apiBase}
+    </span>,
+    document.getElementById("apiBase")
+);
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import './mystyles.scss';
+import './holaMundoReact.tsx';
 import logoImg from './images/logo_2.png';
 
 const img = document.createElement('img');
